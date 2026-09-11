cd114b49d8f758ac4086cc59367d3992c10cf856
GauravTheGreatRepoForAll <156896189+GauravTheGreatRepoForAll@users.noreply.github.com>
2026-09-11 16:46:46 +0530

    Update main.tf

--- Terraform/RootModule/main.tf
+++ Terraform/RootModule/main.tf
@@ -1,17 +1,17 @@
 module "resource_group" {
-  source = "../Child module/Resource group"
+  source = "../../ChileModule/Resource Group"
   resource_group = var.resource_group
 }
 
 module "virtual_network" {
-  source = "../Child module/Virtual Network"
+  source = "../../ChileModule/Virtual Network"
   virtual_network = var.virtual_network
   #resource_group = module.resource_group.rg
   depends_on = [module.resource_group]
 }
 
 module "kubernetes_cluster" {
-  source = "../Child module/Kubernetes Cluster"
+  source = "../../ChileModule/Kubernetes Cluster"
   kubernetes_cluster = var.kubernetes_cluster
   resource_group = module.resource_group.rg
 
