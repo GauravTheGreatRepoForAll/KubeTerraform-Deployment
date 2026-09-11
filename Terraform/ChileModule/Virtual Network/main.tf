84213a749cf2bd8a2fe8c9bc07971c812b7faa82
GauravTheGreatRepoForAll <156896189+GauravTheGreatRepoForAll@users.noreply.github.com>
2026-09-11 17:26:39 +0530

    Update main.tf

--- Terraform/ChileModule/Virtual Network/main.tf
+++ Terraform/ChileModule/Virtual Network/main.tf
@@ -3,8 +3,8 @@ resource "azurerm_virtual_network" "vnet" {
   for_each = var.virtual_network
 
   name                = each.value.vnet_name
-  location            = azurerm_resource_group.rg[each.value.resource_group].location
-  resource_group_name = azurerm_resource_group.rg[each.value.resource_group].name
+  location            = each.value.location
+  resource_group_name = each.value.resource_group
 
   address_space = each.value.address_space
   dns_servers   = each.value.dns_servers
